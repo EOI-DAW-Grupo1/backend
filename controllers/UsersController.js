bba542901402216576aa4ca25a5d229222507569
Fran <franblancoruiz619@gmail.com>
2021-06-02 10:34:07 +0200

    Minor changes un usersController

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -3,14 +3,6 @@
 const express = require('express');
 const { sha512 } = require('js-sha512');
 const router = express.Router()
-<<<<<<< HEAD
-const config = require('../modules/config')
-// const mailer = require('../modules/mailer')
-const authMiddleware = require('../modules/authenticator')
-const onlyRegisteredAccess = authMiddleware(true, ['user', 'admin'])
-const onlyAdminAccess = authMiddleware(true, ['admin'])
-=======
->>>>>>> origin/master
 
 
 const userModel = require('../models/UserModel')
@@ -31,28 +23,6 @@ router.route('/newUser')
     } catch (error) {
       res.status(400).json({ message: error.message })
     }
-<<<<<<< HEAD
-
-    // try {
-    //   //envío email de bienvenida en segundo plano
-    //   userData.public_domain = config.PUBLIC_DOMAIN
-    //   res.render(config.WELLCOME_EMAIL_TPL, userData, async (err, emailBody) => {
-    //     if (err) {
-    //       //si se produce algún error de renderización del template se cancela el envío
-    //       return
-    //     }
-
-    //     const from = { name: userData.firstname, email: userData.email }
-
-    //     //envía correo electrónico
-    //     await mailer.send(from, userData.email, config.WELLCOME_SUBJECT, emailBody, true)
-    //   })
-    // } catch (error) {
-    //   console.info("Envío de correo electrónico al usuario erróneo.")
-    //   console.error(error)
-    // }
-=======
->>>>>>> origin/master
   })
 
 
